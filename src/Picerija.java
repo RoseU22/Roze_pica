17cbdcb722d2df82b86d0141a578a9714b3fdd40
Roze <rozeu668@gmail.com>
2023-03-14 08:55:50 +0200

    Uztaisa programmas pamatu, kas atļaujs ievadīt datus par personu un par picu

--- src/Picerija.java
+++ src/Picerija.java
@@ -1,9 +1,36 @@
+import javax.swing.JOptionPane;
 
 public class Picerija {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		String[] opcijas = {"Ievadît kontaktdatus","Uztaisît picu","Apskatît aktîvos pasûtîjumus","Iziet no programmas"};
+		String izvele;
+		
+		do{
+			izvele = (String)JOptionPane.showInputDialog(null, "Picas pasûtîðanas sistçma", "Picçrija", JOptionPane.INFORMATION_MESSAGE, null, opcijas, opcijas[0]);
+			
+			switch(izvele){
+				
+			case "Ievadît kontaktdatus":
+				
+				break;
+				
+			case "Uztaisît picu":
+				
+				break;
+				
+			case "Apskatît aktîvos pasûtîjumus":
+				
+				break;
+				
+			case "Iziet no programmas":
+				
+				break;
+			
+			}
+			
+		}while(!izvele.equalsIgnoreCase("Iziet no programmas"));
 	}
 
 }
