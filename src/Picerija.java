94cf46cba5e87894afe653779e7126a43df3793b
Roze <rozeu668@gmail.com>
2023-03-15 08:34:46 +0200

    Nomainīju kodu tā, lai lietotājs var ievadīt vairākus datus par sevi vienā JOptionPAne lodziņā, kā arī pievienoju piedevas izvēlēs kurus var izvēlēties izmantojot JCheckBox

--- src/Picerija.java
+++ src/Picerija.java
@@ -3,20 +3,23 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 
+import javax.swing.JCheckBox;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class Picerija {
 
 	static String fNosaukums;
 	
 	
-	static void saglabat(String adrese, String vards, String uzvards, int talrunis){
+	static void saglabat(JTextField Adrese, JTextField Vards, JTextField Uzvards, JTextField Talrunis, JTextField PicasLielums, JTextField Merce, int[] IzveletasOpcijas){
 		fNosaukums = JOptionPane.showInputDialog("Kur glabâsiet kontaktdatus?");
 		
 		try{
 			FileWriter fw = new FileWriter(fNosaukums+".txt", true);
 			PrintWriter pw = new PrintWriter(fw);
-			pw.println(vards+" "+uzvards+": "+adrese+", "+talrunis);
+			//pw.println(vards+" "+uzvards+": "+adrese+", "+talrunis);
 			pw.close();
 			JOptionPane.showMessageDialog(null, "Veiksmîgi tika ierakstîti kontaktdati!");
 		}catch(Exception e){
@@ -41,22 +44,79 @@ public class Picerija {
 		}
 	}
 	
-	static void IevaditKontaktDatus(){
-		String adrese, vards, uzvards;
-		int talrunis;
+	static void UztaisitPicu(){
+		JTextField Adrese = new JTextField();
+		JTextField Vards = new JTextField();
+		JTextField Uzvards = new JTextField();
+		JTextField Talrunis = new JTextField();
+		JTextField PicasLielums = new JTextField();
+		JTextField Merce = new JTextField();
 		
-		adrese = JOptionPane.showInputDialog("Ievadi personas adresi:");
-		vards = JOptionPane.showInputDialog("Ievadi personas vârdu:");
-		uzvards = JOptionPane.showInputDialog("Ievadi personas uzvârdu:");
-		talrunis = Integer.parseInt(JOptionPane.showInputDialog("Ievadi personas tâlruni:"));
+		Object[] Kontaktdati = {
+				"Adrese: ", Adrese,
+				"Vârds: ", Vards,
+				"Uzvards: ", Uzvards,
+				"Talrunis: ", Talrunis
+		};
 		
-		saglabat(adrese, vards, uzvards, talrunis);
+		Object[] Pica = {
+				"Izvçlies picas lielumu (1cm = 0.35€): ", PicasLielums,
+				"Izvçlies picas mçrci:\nAsâ mçrce - 1€\nGuríu mçrce - 1.25€\nÍiploku mçrce - 1.15€\nTomâtu-krçjuma mçrce - 1.50€\nKarija mçrce - 1.30€", Merce
+		};
+		
+		JOptionPane.showConfirmDialog(null, Kontaktdati, "Kontaktdati", JOptionPane.OK_CANCEL_OPTION);
+		JOptionPane.showConfirmDialog(null, Pica, "Picas taisîðana", JOptionPane.OK_CANCEL_OPTION);
+		
+		Object[] piedevas = {"Siers",
+							 "Bekons",
+							 "Ðíiòíis",
+							 "Vistas fileja",
+							 "Maltâ gaïa",
+							 "Sarkanie sîpoli",
+							 "Ðampinjoni",
+							 "Tomâti",
+							 "Paprika"};
+		
+		double[] Cenas = {2.00,1.50,1.50,1.50,1.30,0.70,0.70,0.70,0.50};
+		double summa=0;
+		int[] IzveletasOpcijas = new int[piedevas.length];
+		
+		JPanel panel = new JPanel();
+		for(int i=0; i<piedevas.length; i++){
+			JCheckBox CBox = new JCheckBox(piedevas[i].toString());
+			panel.add(CBox);
+		}
+		
+		int rezultats = JOptionPane.showConfirmDialog(null, panel, "Izvçlies piedevas picai!", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+		
+		if(rezultats==JOptionPane.OK_OPTION){
+			StringBuilder sb = new StringBuilder();
+			for(int i=0; i<panel.getComponentCount(); i++){
+				if(panel.getComponent(i) instanceof JCheckBox){
+					JCheckBox CBox = (JCheckBox) panel.getComponent(i);
+					if(CBox.isSelected()){
+						sb.append(CBox.getText()).append("\n");
+						IzveletasOpcijas[i] = 1;
+						summa += Cenas[i];
+					}
+				}
+			}
+			
+			String teksts = PicasLielums.getText();
+			double prece = Double.parseDouble(teksts);
+			double cena = ((prece*0.35)+summa*1);
+			
+			JOptionPane.showMessageDialog(null, "Paldies par pirkumu! Jûsu maksa par picu: "+cena+"€");
+			
+			saglabat(Adrese, Vards, Uzvards, Talrunis, PicasLielums, Merce, IzveletasOpcijas);
+			
+		}
 		
 	}
 	
 	public static void main(String[] args) {
 		
-		String[] opcijas = {"Ievadît kontaktdatus","Uztaisît picu","Apskatît aktîvos pasûtîjumus","Iziet no programmas"};
+		String[] opcijas = {"Uztaisît picu","Apskatît aktîvos pasûtîjumus","Iziet no programmas"};
 		String izvele;
 		
 		do{
@@ -64,12 +124,8 @@ public class Picerija {
 			
 			switch(izvele){
 				
-			case "Ievadît kontaktdatus":
-				IevaditKontaktDatus();
-				break;
-				
 			case "Uztaisît picu":
-				
+				UztaisitPicu();
 				break;
 				
 			case "Apskatît aktîvos pasûtîjumus":
