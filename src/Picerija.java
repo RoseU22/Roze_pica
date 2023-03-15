df674afe118d415a6d72b0b552a00332495dbde4
Roze <rozeu668@gmail.com>
2023-03-15 12:12:14 +0200

    Atkal dažu ķļūdu labojumi

--- src/Picerija.java
+++ src/Picerija.java
@@ -55,8 +55,6 @@ public class Picerija {
 	static void apskatitPasutijumus(){
 		String teksts, str="";
 		
-		fNosaukums = JOptionPane.showInputDialog("Kuru failu gribat apskatît?");
-		
 		try{
 			FileReader fr = new FileReader(fNosaukums+".txt");
 			BufferedReader br = new BufferedReader(fr);
@@ -74,7 +72,6 @@ public class Picerija {
 	static void UztaisitPicu(){
 		
 		double cena = 0;
-		double piegade=0;
 		
 		JTextField Adrese = new JTextField();
 		JTextField Vards = new JTextField();
@@ -88,6 +85,8 @@ public class Picerija {
 		
 		if(OpcijasIzvele == 0){
 			
+		
+		
 		Object[] Kontaktdati = {
 				"Adrese: ", Adrese,
 				"Vârds: ", Vards,
@@ -96,7 +95,7 @@ public class Picerija {
 		};
 		
 		JOptionPane.showConfirmDialog(null, Kontaktdati, "Kontaktdati", JOptionPane.OK_CANCEL_OPTION);
-		piegade += 1.25;
+		cena += 1.25;
 		
 		}else if(OpcijasIzvele==1){
 			
@@ -183,7 +182,7 @@ public class Picerija {
 					}
 				}
 			
-			JOptionPane.showMessageDialog(null, "Paldies par pirkumu! Jûsu maksa par picu: "+(cena+sum+piegade)+"€");
+			JOptionPane.showMessageDialog(null, "Paldies par pirkumu! Jûsu maksa par picu: "+(cena+sum)+"€");
 			}
 		
 			saglabat(Adrese, Vards, Uzvards, Talrunis, PicasLielums, IzveletasOpcijas, IzvOpcijas, OpcijasIzvele);
@@ -196,6 +195,7 @@ public class Picerija {
 		String izvele;
 		
 		do{
+			
 			izvele = (String)JOptionPane.showInputDialog(null, "Picas pasûtîðanas sistçma", "Picçrija", JOptionPane.INFORMATION_MESSAGE, null, opcijas, opcijas[0]);
 			
 			switch(izvele){
